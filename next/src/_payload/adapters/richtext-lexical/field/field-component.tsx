9150e1502df2777923986b0788d728f881fa68c1
Anthony Bouch <anthony@infonomic.io>
2025-04-05 00:16:38 +0700

    feat: more work on updated lexical editor

--- next/src/_payload/adapters/richtext-lexical/field/field-component.tsx
+++ next/src/_payload/adapters/richtext-lexical/field/field-component.tsx
@@ -3,18 +3,16 @@
  * Adapted from on https://github.com/payloadcms/payload/blob/main/packages/richtext-lexical/src/field/Field.tsx
  */
 
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
-
+import React, { useCallback, useMemo, useState, useEffect } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
+import type { EditorState, LexicalEditor, SerializedEditorState } from 'lexical'
 
-import { FieldLabel, useEditDepth, useField, withCondition } from '@payloadcms/ui'
-
+import { FieldLabel, useEditDepth, useField, useEffectEvent, RenderCustomComponent, FieldError, FieldDescription } from '@payloadcms/ui'
 import { mergeFieldStyles } from '@payloadcms/ui/shared'
 
 import { richTextValidate } from '../validate/validate-server'
 import { EditorContext } from './editor-context'
 
-import type { EditorState, LexicalEditor, SerializedEditorState } from 'lexical'
 import type { LexicalRichTextFieldProps } from '../types'
 
 import './field-component.scss'
@@ -29,7 +27,7 @@ const RichTextComponent: React.FC<LexicalRichTextFieldProps> = (props) => {
     field,
     field: {
       name,
-      admin: { className, readOnly: readOnlyFromAdmin } = {},
+      admin: { className, description, readOnly: readOnlyFromAdmin } = {},
       label,
       localized,
       required
@@ -41,8 +39,6 @@ const RichTextComponent: React.FC<LexicalRichTextFieldProps> = (props) => {
 
   const readOnlyFromProps = readOnlyFromTopLevelProps || readOnlyFromAdmin
   const path = pathFromProps ?? name
-
-  const [statefulReadonly, setStatefulReadonly] = useState<boolean>(false)
 
   const editDepth = useEditDepth()
 
@@ -63,8 +59,7 @@ const RichTextComponent: React.FC<LexicalRichTextFieldProps> = (props) => {
 
   const {
     customComponents: { AfterInput, BeforeInput, Description, Error, Label } = {},
-    formInitializing,
-    formProcessing,
+    disabled: disabledFromField,
     initialValue,
     setValue,
     showError,
@@ -74,22 +69,19 @@ const RichTextComponent: React.FC<LexicalRichTextFieldProps> = (props) => {
     validate: memoizedValidate
   })
 
-  // console.log('readOnlyFromProps:', readOnlyFromProps)
-  // console.log('formInitializing:', formInitializing)
-  // console.log('formProcessing:', formProcessing)
+  const disabled = readOnlyFromProps || disabledFromField
 
-  useEffect(() => {
-    setStatefulReadonly(readOnlyFromProps || formProcessing || formInitializing)
-  }, [readOnlyFromProps, formProcessing, formInitializing])
-  // const disabled = readOnlyFromProps || formProcessing || formInitializing
-  // const disabled = readOnlyFromProps || false // || formProcessing || formInitializing
+  const [rerenderProviderKey, setRerenderProviderKey] = useState<Date>()
+
+  const prevInitialValueRef = React.useRef<SerializedEditorState | undefined>(initialValue)
+  const prevValueRef = React.useRef<SerializedEditorState | undefined>(value)
 
   const classes = [
     baseClass,
     'field-type',
     className,
     showError && 'error',
-    statefulReadonly && `${baseClass}--read-only`,
+    disabled && `${baseClass}--read-only`,
     admin?.hideGutter !== true ? `${baseClass}--show-gutter` : null
   ]
     .filter(Boolean)
@@ -97,46 +89,87 @@ const RichTextComponent: React.FC<LexicalRichTextFieldProps> = (props) => {
 
   const pathWithEditDepth = `${path}.${editDepth}`
 
+  const handleChange = useCallback(
+    (editorState: EditorState) => {
+      const newState = editorState.toJSON()
+      prevValueRef.current = newState
+      setValue(newState)
+    },
+    [setValue],
+  )
+
   const styles = useMemo(() => mergeFieldStyles(field), [field])
+
+  const handleInitialValueChange = useEffectEvent(
+    (initialValue: SerializedEditorState | undefined) => {
+      // Object deep equality check here, as re-mounting the editor if
+      // the new value is the same as the old one is not necessary
+      if (
+        prevValueRef.current !== value &&
+        JSON.stringify(prevValueRef.current) !== JSON.stringify(value)
+      ) {
+        prevInitialValueRef.current = initialValue
+        prevValueRef.current = value
+        setRerenderProviderKey(new Date())
+      }
+    },
+  )
+
+  useEffect(() => {
+    // Needs to trigger for object reference changes - otherwise,
+    // reacting to the same initial value change twice will cause
+    // the second change to be ignored, even though the value has changed.
+    // That's because initialValue is not kept up-to-date
+    if (!Object.is(initialValue, prevInitialValueRef.current)) {
+      handleInitialValueChange(initialValue)
+    }
+  }, [initialValue])
 
   return (
     <div className={classes} key={pathWithEditDepth} style={styles}>
       <div className={`${baseClass}__wrap`}>
-        {Error}
-        {Label || <FieldLabel label={label} localized={localized} required={required} />}
+        <RenderCustomComponent
+          CustomComponent={Error}
+          Fallback={<FieldError path={path} showError={showError} />}
+        />
+        {Label || <FieldLabel label={label} localized={localized} path={path} required={required} />}
         <ErrorBoundary fallbackRender={fallbackRender} onReset={() => {}}>
           {BeforeInput}
           <EditorContext
             editorConfig={editorConfig}
             fieldProps={props}
             path={name}
-            readOnly={statefulReadonly}
+            key={JSON.stringify({ path, rerenderProviderKey })} // makes sure lexical is completely re-rendered when initialValue changes, bypassing the lexical-internal value memoization. That way, external changes to the form will update the editor. More infos in PR description (https://github.com/payloadcms/payload/pull/5010)
+            onChange={handleChange}
+            readOnly={disabled}
             value={value}
-            key={JSON.stringify({ initialValue, name })} // makes sure lexical is completely re-rendered when initialValue changes, bypassing the lexical-internal value memoization. That way, external changes to the form will update the editor. More infos in PR description (https://github.com/payloadcms/payload/pull/5010)
             // NOTE: 2023-05-15 disabled the deepEqual since we've set ignoreSelectionChange={true}
             // in our OnChangePlugin instances - and so a call here means that something
             // must have changed - so no need to do the comparison.
-            onChange={(editorState: EditorState, editor: LexicalEditor, tags: Set<string>) => {
-              if (!statefulReadonly) {
-                const serializedEditorState = editorState.toJSON()
-                // TODO: 2024-01-30 - re-test this.
-                // NOTE: 2023-06-28 fix for setValue below. For some reason when
-                // this custom field is used in a block field, setValue on its
-                // own won't enable Save Draft or Publish Changes during a first
-                // add of a new block (it will after the entire document is saved
-                // and reloaded - but not before.) So call setModified(true) here
-                // to guarantee that we can always save our changes.
-                // setModified(true)
-                // NOTE: 2024-05-02: Appears to be fixed - and setModified(true)
-                // is no longer required.
-                setValue(serializedEditorState)
-              }
-            }}
+            // onChange={(editorState: EditorState, editor: LexicalEditor, tags: Set<string>) => {
+            //   if (!disabled) {
+            //     const serializedEditorState = editorState.toJSON()
+            //     // TODO: 2024-01-30 - re-test this.
+            //     // NOTE: 2023-06-28 fix for setValue below. For some reason when
+            //     // this custom field is used in a block field, setValue on its
+            //     // own won't enable Save Draft or Publish Changes during a first
+            //     // add of a new block (it will after the entire document is saved
+            //     // and reloaded - but not before.) So call setModified(true) here
+            //     // to guarantee that we can always save our changes.
+            //     // setModified(true)
+            //     // NOTE: 2024-05-02: Appears to be fixed - and setModified(true)
+            //     // is no longer required.
+            //     setValue(serializedEditorState)
+            //   }
+            // }}
           />
           {AfterInput}
         </ErrorBoundary>
         {Description}
-        {/* <FieldDescription description={description as string} {...descriptionProps} /> */}
+        <RenderCustomComponent
+          CustomComponent={Description}
+          Fallback={<FieldDescription description={description} path={path} />}
+        />
       </div>
     </div>
   )
@@ -152,4 +185,4 @@ function fallbackRender({ error, resetErrorBoundary }: any): React.JSX.Element {
   )
 }
 
-export const RichText: typeof RichTextComponent = withCondition(RichTextComponent)
+export const RichText: typeof RichTextComponent = RichTextComponent
